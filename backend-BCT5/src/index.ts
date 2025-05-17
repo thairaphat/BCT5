d29a65da15f640d3241a55bfe858416fe34da838
thairaphat <12758thiraphat@gmail.com>
2025-05-17 10:35:38 -0700

    setcore

--- backend-BCT5/src/index.ts
+++ backend-BCT5/src/index.ts
@@ -19,7 +19,7 @@ async function startApp() {
 
   const app = new Elysia()
     .use(cors({
-        origin: 'http://localhost:5173', 
+        origin: 'http://localhost:5174', 
         credentials: true                
       }))
   .group('/api', app => {
